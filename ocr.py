698760bf6c7e619af43b12e92bb853c06e37e8a5
Alex Zhu <alex.zhu@MacBook-Pro-4.local>
2018-06-03 14:24:05 -0400

    seperate quesiton and answer

--- ocr.py
+++ ocr.py
@@ -19,17 +19,29 @@ ap.add_argument("-p", "--preprocess", type=str, default="thresh",
                 help="type of preprocessing to be done")
 args = vars(ap.parse_args())
 
+question_x = 120
+question_y = 200
+question_width = 250
+question_length = 150
+
+answers_x = question_x
+answers_y = question_y + question_length
+answers_width = question_width
+answers_length = 200
+
+stop_words = ["which ", "what ", "who ","has ", "have ", "had ",
+				"is ", "are ", "was ", "were ",
+				"these ", "those ", "this ", "that ", "the ",
+				"of ", "a ", "an ", "?"]
 
 def captureScreen(x, y, wide, length, output):
     subprocess.run(
         ["screencapture", "-R{},{},{},{}".format(x, y, wide, length), output])
 
-
-def ocr(image_file):
+def ocr(image_file, tmp_file_name):
 	# load the example image and convert it to grayscale
 	image = cv2.imread(image_file)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 	# check to see if we should apply thresholding to preprocess the
 	# image
 	if args["preprocess"] == "thresh":
@@ -43,10 +55,9 @@ def ocr(image_file):
 
 	# write the grayscale image to disk as a temporary file so we can
 	# apply OCR to it
-	filename = "{}.png".format(os.getpid())
-	cv2.imwrite(filename, gray)
-	text = pytesseract.image_to_string(Image.open(filename))
-	os.remove(filename)
+	cv2.imwrite(tmp_file_name, gray)
+	text = pytesseract.image_to_string(Image.open(tmp_file_name))
+	#os.remove(filename)
 	lines = text.splitlines()
 	return lines
 
@@ -60,37 +71,46 @@ def rlinput(prompt, prefill=''):
 
 
 # step 2: extract & clean question and answers
-def extractNclean(lines):
+def extractQuestion(lines):
 	question = ""
-	answers = []
 	# extract
 	is_question_done = False
 	for line in lines:
 		line = line.strip().lower()
 		if len(line) > 3:
-			if is_question_done:
-				answers.append(line)
-			else:
-				question += " " + line
-				if line.find('?') != -1:
-					is_question_done = True
+			question += " " + line
+			if line.find('?') != -1:
+				is_question_done = True
 	# clean
-	stop_words = ["which ", "what ", "has ", "have ", "had ",
-				"is ", "are ", "was ", "were ",
-				"these ", "those ", "this ", "that ",
-				"of ", "a ", "an ", "?"]
 	for word in stop_words:
 		question = question.replace(word, "")
 	confirmed = rlinput("Confirm the question :", question)
 	print("question = {}".format(confirmed))
+	return confirmed
 
+def extractAnswers(lines):
+	answers = []
+	for line in lines:
+		line = line.strip().lower()
+		if len(line) > 3:
+			answers.append(line)
+	
 	for i, ans in enumerate(answers):
 		print("ans{} = {}".format(i, ans))
-	print("\n")
-	return confirmed, answers
+	return answers	
 
 # step 3: using custom search api question + answer
+# drivers = []
+# desiredWidth = (1851+70)/3; desiredHeight = 1060; initialX = 69; initialY = 23
+# for i in range(0,3,1):
+#     driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
+#     driver.set_window_rect(initialX + desiredWidth * i, initialY,desiredWidth,desiredHeight)
+#     drivers.append(driver)
+
+
+
 def searchNcount(question, answers):
+
 	for i, ans in enumerate(answers):
 		url = "https://www.googleapis.com/customsearch/v1"
 		param = {
@@ -102,7 +122,16 @@ def searchNcount(question, answers):
 		r = requests.get(url, param)
 		res = json.loads(r.content)
 		print ("search question+ans{} : num of results{}".format(i, res["searchInformation"]["totalResults"]))
-		#print(json.dumps(res,indent=4))
+		if not res["items"]:
+			print("ans {} no result".format(i))
+		else:
+			print ("ans {} top record: {}".format(i, res["items"][0]["title"]))	
+		# driverTemp = drivers[i]
+		# driverTemp.switch_to_window(driverTemp.current_window_handle) #bring to foreground 
+		# driverTemp.get('http://www.google.com/search?q={}'.format(question+" "+ans));
+		# driverTemp.execute_script("document.body.style.zoom='90%'") #zoom to show more content
+		# driverTemp.execute_script("window.scrollTo(130, 100)")
+		# #print(json.dumps(res,indent=4))
 
 # for ans in answers:
 # 	driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
@@ -112,13 +141,15 @@ def searchNcount(question, answers):
 # driver.quit()
 
 def solveQuestion(image_name):
-	print("step 1: OCR")
-	captureScreen(20, 100, 500, 500, image_name)
-	lines = ocr(image_name)
-	print("image content:{}".format(lines))
+	captureScreen(question_x, question_y, question_width, question_length, "question.png")
+	lines = ocr("question.png", "q_tmp.png")
+	print("question content:{}".format(lines))
+	question = extractQuestion(lines)
 
-	print("step 2: extracting info")
-	question, answers = extractNclean(lines)
+	captureScreen(answers_x, answers_y, answers_width, answers_length, "answers.png")
+	lines = ocr("answers.png", "a_tmp.png")
+	print("answers content:{}".format(lines))
+	answers = extractAnswers(lines)
 
 	print("step 3: search google")
 	searchNcount(question, answers)
@@ -138,3 +169,5 @@ def winHqTrivia():
 		print("next question\n")
 
 winHqTrivia()
+# for driver in drivers:
+# 	driver.quit()
